6b28da77cdb775039812aa6c2d1757e06e2ca22b
Mario Zambrano <80787742+MarioZ1204@users.noreply.github.com>
2024-04-22 22:15:52 -0500

    Visualizar Ruta

---
+++ rutas/ruta/back/visualizar.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Visualizar Ruta</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="../front/img/favicon.png" rel="icon">
+  <link href="../front/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Roboto:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Work+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="../front/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../front/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="../front/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="../front/vendor/aos/aos.css" rel="stylesheet">
+  <link href="../front/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="../front/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="../front/css/style_recorrido.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: UpConstruction
+  * Template URL: https://bootstrapmade.com/upconstruction-bootstrap-construction-website-template/
+  * Updated: Mar 17 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="header d-flex align-items-center">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+
+      <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+      <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a href="../../../index.html" class="active">Home</a></li>
+      </nav><!-- .navbar -->
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto" href="#contact">Volver</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav>
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="hero">
+
+    <div class="info d-flex align-items-center">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-6 text-center">
+            <h2 data-aos="fade-down">Ruta <br><span>Castellana - Goretti</span></h2>
+            <p data-aos="fade-up">Tiempo estimado: 10 min</p></h4>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div id="hero-carousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
+
+      <div class="carousel-item active" style="background-image: url(../front/img/index.png)"></div>
+
+    </div>
+
+  </section><!-- End Hero Section -->
+
+  <main id="main">
+
+<!-- ======= Our Projects Section ======= -->
+<section id="installation-info" class="installation-info">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-6">
+            <img src="../front\img\Castellana - Goretti-01.png" class="img-fluid" alt="San Francisco" style="margin-top: 50px;">
+          </div>
+        </div>
+        <div class="row justify-content-center mt-4">
+          <div class="col-md-8">
+            <h3>Descripción</h3>
+            <p>Su punto de partida es la Puerta de la Castellana, para llegar a su destino, proceda a ingresar por la puerta y subir el pasillo, al terminar el pasillo podrá encontrar el parqueadero, gire levemente a la derecha y siga caminando, al terminar el parqueadero podrá encontrar las canchas ubicadas al frente del Coliseo de Guillermo de Castellana, siga caminando en dirección recta hasta llegar al pasillo que se encuentra a un lado de la cafetería, camine por el pasillo y al final encontrará la puerta del Hall del edificio San Francisco (Sede A), camine en dirección recta hasta llegar a las gradas que se encuentran a un lado del ascensor, proceda a bajar las gradas, al terminar, al frente encontrará las puertas, proceda a salir y a bajar las siguientes gradas, al terminar encontrará su destino.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+
+
+    <!-- ======= Testimonials Section ======= -->
+    <section id="testimonials" class="testimonials section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-header">
+          <h2>Anuncios</h2>
+          </div>
+
+        <div class="slides-2 swiper">
+          <div class="swiper-wrapper">
+
+            <div class="swiper-slide">
+              <div class="testimonial-wrap">
+                <div class="testimonial-item">
+                  <img src="../front/img/testimonials/testimonials-1.jpg" class="testimonial-img" alt="">
+                  <h3>Saul Goodman</h3>
+                  <h4>Ceo &amp; Founder</h4>
+                  <div class="stars">
+                    <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
+                  </div>
+                  <p>
+                    <i class="bi bi-quote quote-icon-left"></i>
+                    Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
+                    <i class="bi bi-quote quote-icon-right"></i>
+                  </p>
+                </div>
+              </div>
+            </div><!-- End testimonial item -->
+
+            <div class="swiper-slide">
+              <div class="testimonial-wrap">
+                <div class="testimonial-item">
+                  <img src="../front/img/testimonials/testimonials-2.jpg" class="testimonial-img" alt="">
+                  <h3>Sara Wilsson</h3>
+                  <h4>Designer</h4>
+                  <div class="stars">
+                    <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
+                  </div>
+                  <p>
+                    <i class="bi bi-quote quote-icon-left"></i>
+                    Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.
+                    <i class="bi bi-quote quote-icon-right"></i>
+                  </p>
+                </div>
+              </div>
+            </div><!-- End testimonial item -->
+          </div>
+          <div class="swiper-pagination"></div>
+        </div>
+
+      </div>
+    </section><!-- End Testimonials Section -->
+
+<!-- ======= Footer ======= -->
+<footer id="footer" class="footer">
+  <div class="container">
+    <div class="copyright">
+    </div>
+  </div>
+</footer><!-- End  Footer -->
+
+  <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="../front/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../front/vendor/aos/aos.js"></script>
+  <script src="../front/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="../front/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="../front/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="../front/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="../front/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="../front/js/main.js"></script>
+
+</body>
+
+</html>
