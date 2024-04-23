cfad5529ff362f5d65d0ef1f71fb284d8b47085f
Mario Zambrano <80787742+MarioZ1204@users.noreply.github.com>
2024-04-23 11:08:16 -0500

    Listar Punto

---
+++ rutas/punto/back/listarP.php
@@ -0,0 +1,171 @@
+<!DOCTYPE php>
+<php lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>Puntos</title>
+
+
+  <!-- Favicons -->
+  <link href="../front/img/flavicon-01.png" rel="icon">
+  <link href="../front/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="../front/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="../front/vendor/aos/aos.css" rel="stylesheet">
+  <link href="../front/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../front/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="../front/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="../front/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="../front/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="../front/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="../front/css/style_listarP.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Anyar
+  * Template URL: https://bootstrapmade.com/anyar-free-multipurpose-one-page-bootstrap-theme/
+  * Updated: Mar 17 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Top Bar ======= -->
+  <div id="topbar" class="fixed-top d-flex align-items-center ">
+    <div class="container d-flex align-items-center justify-content-center justify-content-md-between">
+      <div class="contact-info d-flex align-items-center">
+        <i class="bi bi-envelope-fill"></i><a href="mailto:contact@example.com">encryption@gmail.com</a>
+        <i class="bi bi-phone-fill phone-icon"></i> +57 3164518932
+      </div>
+      <div class="cta d-none d-md-block">
+      </div>
+    </div>
+  </div>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center ">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <h1 class="logo"><a href="listar.php">CESMAPS</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href=listar.php" class="logo"><img src="front/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto" href="#contact">Volver</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+    <section id="hero" class="d-flex justify-cntent-center align-items-center">
+    <div id="heroCarousel" data-bs-interval="5000" class="container carousel carousel-fade" data-bs-ride="carousel">
+
+      <!-- Slide 1 -->
+      <div class="carousel-item active">
+        <div class="carousel-container">
+          <h2 class="animate__animated animate__fadeInDown">Puntos <span></span></h2>
+          <p class="animate__animated animate__fadeInUp">Aquí se visualizarán los puntos que se encuentran en nuestra universidad</p>
+        </div>
+      </div>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+
+
+
+    <!-- ======= Icon Boxes Section ======= -->
+    <section id="icon-boxes" class="icon-boxes">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="fade-up">
+            <div class="icon-box">
+              <h4 class="title"><a href="punto1.php">Entrada Castellana
+              <h4 class="title"><a href=""></a></h4>
+              <img src="../front\img\Puerta Castellana.png" style="max-width: 100%;">
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="fade-up" data-aos-delay="100">
+            <div class="icon-box">
+              <h4 class="title"><a href="rutas\instalacion\back\ins2.php">Entrada Goretti</B>
+              <h4 class="title"><a href=""></a></h4>
+              <img src="../front\img\Puerta Goretti.png" style="max-width: 100%;">
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="fade-up" data-aos-delay="100">
+            <div class="icon-box">
+              <h4 class="title"><a href="rutas\instalacion\back\ins3.php">Crédito Y Cartera
+              <h4 class="title"><a href=""></a></h4>
+              <img src="../front\img\Crédito y Cartera.png" style="max-width: 100%;">
+            </div>
+          </div>
+          
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="fade-up" data-aos-delay="100">
+            <div class="icon-box">
+              <h4 class="title"><a href="rutas\instalacion\back\ins4.php">Tesorería
+              <h4 class="title"><a href=""></a></h4>
+              <img src="../front\img\Tesorería.png" style="max-width: 100%;">
+            </div>
+          </div>
+
+
+          <div class="row mt-5">
+            <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+              <!-- Este div vacío crea el espacio -->
+            </div>
+          </div>
+
+
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="fade-up" data-aos-delay="100">
+            <div class="icon-box">
+              <h4 class="title"><a href="rutas\instalacion\back\ins5.php">Cafetería
+              <h4 class="title"><a href=""></a></h4>
+              <img src="../front\img\Cafetería.png" style="max-width: 100%;">
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Icon Boxes Section -->
+
+  </main>
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+    <div class="container">
+      <div class="copyright">
+        &copy; Derechos de autor <strong><span>Encryption</span></strong>. Todos los derechos reservados
+      </div>
+    </div>
+  </footer><!-- End  Footer -->
+
+  <!-- Vendor JS Files -->
+  <script src="../front/vendor/aos/aos.js"></script>
+  <script src="../front/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../front/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="../front/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="../front/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="../front/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="../front/js/main.js"></script>
+
+</body>
+
+</php>
---
+++ rutas/punto/back/punto1.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <link href="../front/img/flavicon-01.png" rel="icon">
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>Entrada Castellana</title>
+  <link href="../front/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../front/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="../front/css/style_listarP.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <h1 class="logo"><a href="listar.php">CESMAPS</a></h1>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto" href="listar.php">Volver</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <main id="main">
+
+    <!-- ======= Hero Section ======= -->
+    <section id="hero" class="d-flex justify-content-center align-items-center">
+      <div id="heroCarousel" data-bs-interval="5000" class="container carousel carousel-fade" data-bs-ride="carousel">
+
+        <!-- Slide 1 -->
+        <div class="carousel-item active">
+          <div class="carousel-container">
+            <h2 class="animate__animated animate__fadeInDown">Entrada Castellana</h2>
+          </div>
+        </div>
+      </div>
+    </section><!-- End Hero -->
+
+    <!-- ======= Installation Info Section ======= -->
+    <section id="installation-info" class="installation-info">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-6">
+            <img src="../front\img\Puerta Castellana.png" class="img-fluid" alt="San Francisco" style="margin-top: 50px;">
+          </div>
+        </div>
+        <div class="row justify-content-center mt-4">
+          <div class="col-md-8">
+            <h3>Entrada Gastellana</h3>
+            <h2>Sede Administriva</h2>
+            <p>La entrada principal de la universidad CESMAG está ubicada en la Carrera 20 A 14 – 54 Pasto, Nariño, la universidad cuenta con dos jornadas: la jornada de la mañana inicia a las 6:00 de la mañana y termina a las 2:30 de la tarde, la jornada de la tarde inicia a las 2:30 de la tarde y termina a las 10:30 de la noche. Los vigilantes encargados de las puertas son: 2 en la entrada principal, 2 en la entrada del edificio San Francisco y 2 en el parqueadero, dentro de la universidad existen 2 ronderos en el edificio San Francisco y 2 ronderos en el edificio Holanda.</p>
+          </div>
+        </div>
+      </div>
+    </section><!-- End Installation Info Section -->
+
+
+  </main>
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+    <div class="container">
+      <div class="copyright">
+        &copy; Derechos de autor <strong><span>Encryption</span></strong>. Todos los derechos reservados
+      </div>
+    </div>
+  </footer><!-- End  Footer -->
+
+  <!-- Vendor JS Files -->
+  <script src="../front/vendor/aos/aos.js"></script>
+  <script src="../front/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../front/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="../front/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="../front/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="../front/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="../front/js/main.js"></script>
+
+</body>
+
+</html>
