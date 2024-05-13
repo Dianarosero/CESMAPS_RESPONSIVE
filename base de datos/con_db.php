9fcc7fe72747f28f02b5e7f69d23efaf0eeeef31
Diana Rosero <79727518+Dianarosero@users.noreply.github.com>
2024-05-13 18:42:02 -0500

    bd

--- base de datos/con_db.php
+++ base de datos/con_db.php
@@ -1,3 +1,15 @@
 <?php
-    $conex = mysqli_connect("localhost","root","","cesmaps");
+$host = 'localhost';
+$dbname = 'u814985592_cesmaps';
+$username = 'u814985592_cesmaps';
+$password = '12345Cesmaps';
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    // Establecer el modo de error de PDO a excepción
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexión exitosa";
+} catch(PDOException $e) {
+    echo "Error de conexión: " . $e->getMessage();
+}
 ?>
