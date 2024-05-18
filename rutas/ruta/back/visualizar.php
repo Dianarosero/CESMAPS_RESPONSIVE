2ae1988653ab9e998288bdff2a28860e991db80c
Diana Rosero <79727518+Dianarosero@users.noreply.github.com>
2024-05-18 12:26:18 -0500

    audio visualizar

--- rutas/ruta/back/visualizar.php
+++ rutas/ruta/back/visualizar.php
@@ -104,6 +104,9 @@ if ($result->num_rows > 0) {
   <link href="../front/recorrido/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
   <!-- Template Main CSS File -->
   <link href="../front/recorrido/css/style_recorrido.css" rel="stylesheet">
+  <!-- Bootstrap CSS -->
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+
   <!-- =======================================================
   * Template Name: UpConstruction
   * Template URL: https://bootstrapmade.com/upconstruction-bootstrap-construction-website-template/
@@ -162,6 +165,17 @@ if ($result->num_rows > 0) {
           </div>
         </div>
         <div class="row justify-content-center mt-4">
+        <div class="container mt-5">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Reproductor de Audio</h5>
+                <audio controls class="w-100">
+                    <source src="synthesis.wav" type="audio/wav">
+                    Tu navegador no soporta el elemento de audio.
+                </audio>
+            </div>
+        </div>
+    </div>
           <div class="col-md-8">
             <h3>Descripción</h3>
             <p><?php echo $descripcion; ?></p>
@@ -238,5 +252,9 @@ if ($result->num_rows > 0) {
   <script src="../front/recorrido/vendor/php-email-form/validate.js"></script>
   <!-- Template Main JS File -->
   <script src="../front/recorrido/js/main.js"></script>
+  <!-- Bootstrap JS and dependencies -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
